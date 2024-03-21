b6fa411dbcac29e97493d9687f69729585d8cb71
HenryArpon <henryb.arpon@gmail.com>
2024-03-21 13:18:48 +0800

    add a print pdf button

--- public/script/dashboard.js
+++ public/script/dashboard.js
@@ -2,20 +2,23 @@ document.addEventListener('DOMContentLoaded', () => {
     // Get references to the filter select elements
     const timeframeSelect = document.getElementById("timeframe");
     const yearlyFilter = document.querySelector(".yearly-filter");
-    const quarterlyFilter = document.querySelector(".quarterly-filter");
+    const monthlyFilter = document.querySelector(".monthly-filter");
     const dailyFilter = document.querySelector(".daily-filter");
     const yearSelect = document.getElementById("year");
-    const quarterSelect = document.getElementById("quarter");
+    const monthSelect = document.getElementById("monthly");
     const formFilter = document.getElementById("form-filter");
     const salesDisplay = document.getElementById("sales-display");
     const quantityDisplay = document.getElementById("quantity-display");
     const rangeFilter = document.getElementById("range-filter");
     const applyFilterButton = document.getElementById("apply-filter");
     const timeframeDisplay = document.querySelector(".timeframe_display");
+    const totalDisplay = document.querySelector(".total_display");
+    const pdfButton = document.getElementById('generate-pdf-btn')
     let calendarInput = document.getElementById('calendar');
 
     const currentYear = new Date().getFullYear();
     const endYear = currentYear + 20; 
+    let pdfFileName;
 
 //********************************************************************************
 ///Filter Handler
@@ -23,20 +26,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function toggleFilterVisibility() {
         const selectedTimeframe = timeframeSelect.value;
-
+        
         yearlyFilter.style.display = "none";
-        quarterlyFilter.style.display = "none";
+        monthlyFilter.style.display = "none";
         dailyFilter.style.display = "none";
 
         if (selectedTimeframe === "yearly") {
             yearlyFilter.style.display = "block";
         } 
-        else if (selectedTimeframe === "quarterly") {
-            quarterlyFilter.style.display = "block";
+        else if (selectedTimeframe === "monthly") {
+            monthlyFilter.style.display = "block";
         } 
         else if (selectedTimeframe === "daily") {
             dailyFilter.style.display = "block";
-            applyFilterButton.disabled = true;
+            // applyFilterButton.disabled = true;
 
             rangeFilter.addEventListener('change', () => {
                 applyFilterButton.disabled = false;
@@ -54,10 +57,14 @@ document.addEventListener('DOMContentLoaded', () => {
     for (let year = currentYear; year <= endYear; year++) {
         const yearOption = createOption(year, year);
         yearSelect.appendChild(yearOption);
-
-        const quarterOption = createOption(year, year); 
-        quarterSelect.appendChild(quarterOption);
     }
+
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    months.forEach((month, index) => {
+        const monthOption = createOption(index + 1, month);
+        monthSelect.appendChild(monthOption);
+    });
 
     timeframeSelect.addEventListener("change", toggleFilterVisibility);
      
@@ -77,8 +84,8 @@ document.addEventListener('DOMContentLoaded', () => {
         if (timeframe === 'daily') {
             // params.date = document.getElementById('calendar').value;
             params.date = calendarInput.value;
-        } else if (timeframe === 'quarterly') {
-            params.quarter = document.getElementById('quarter').value;
+        } else if (timeframe === 'monthly') {
+            params.monthly = document.getElementById('monthly').value;
         } else if (timeframe === 'yearly') {
             params.year = document.getElementById('year').value;
         }
@@ -87,6 +94,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const queryString = new URLSearchParams(params).toString();
 
         const getSalesDataURL = `/getSalesData?${queryString}`;
+
         try {
             const response = await fetch(getSalesDataURL, {
                 method: 'GET',
@@ -124,37 +132,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function populateTable(salesData, timeframe) {
         const tableBody = document.getElementById('myTable').getElementsByTagName('tbody')[0];
-
+        let monthIndex = document.getElementById('monthly').value;
+        let totalSales = 0;
+    
         // Clear existing rows
         tableBody.innerHTML = '';
-
-        let totalSales = 0;
-
+    
         // Loop through each sale object in the salesData array
         salesData.forEach(sale => {
-
+    
             // Loop through each item in the sale object
             sale.items.forEach(item => {
-
+    
                 const row = document.createElement('tr');
-
+    
                 // Create cells for each column
                 const skuCell = document.createElement('td');
                 skuCell.textContent = item.sku;
                 row.appendChild(skuCell);
-
+    
                 const productNameCell = document.createElement('td');
                 productNameCell.textContent = item.productName;
                 row.appendChild(productNameCell);
-
+    
                 const quantityCell = document.createElement('td');
                 quantityCell.textContent = item.quantity;
                 row.appendChild(quantityCell);
-
+    
                 const priceCell = document.createElement('td');
                 priceCell.textContent = item.price
                 row.appendChild(priceCell);
-
+    
                 const totalPriceCell = document.createElement('td');
                 totalPriceCell.textContent = item.price * item.quantity;
                 row.appendChild(totalPriceCell);
@@ -162,11 +170,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 // Append the row to the table body
                 tableBody.appendChild(row);
             });
-
+    
             totalSales += sale.totalPrice;
         });
-
-        // Display timeframe and total sales
+    
         if (timeframe === "daily") {
             const currentDate = new Date();
             const formattedDate = currentDate.toLocaleDateString('en-US', {
@@ -176,17 +183,25 @@ document.addEventListener('DOMContentLoaded', () => {
             });
             
             timeframeDisplay.textContent = `Daily Sales Report as of ${formattedDate}`;
+            pdfFileName = timeframeDisplay.textContent;
+        }
+        else if (timeframe === "monthly") {
+            const currentMonth = months[monthIndex -1]; // Get the month name from the array using the monthIndex
+            const currentYear = new Date().getFullYear();
+            timeframeDisplay.textContent = `Monthly Sales Report for ${currentMonth} ${currentYear}`;
+            pdfFileName = timeframeDisplay.textContent;
         }
         else if (timeframe === "yearly") {
-            const currentDate = new Date();
-            const year = currentDate.getFullYear();
-            timeframeDisplay.textContent = `Annual Sales Report for ${year}`;
+            const currentYear = new Date().getFullYear();
+            timeframeDisplay.textContent = `Annual Sales Report for ${currentYear}`;
+            pdfFileName = timeframeDisplay.textContent;
         }
         
         salesDisplay.textContent = `Timeframe: ${capitalizeFirstLetter(timeframe)}`;
         quantityDisplay.textContent = `Total Sales: ${currencyFormatter.format(totalSales)}`;
+        totalDisplay.textContent = `TOTAL SALES: ${currencyFormatter.format(totalSales)}`;
     }
-
+    
 //********************************************************************************
 ///Auto load chart updates
 //********************************************************************************
@@ -197,6 +212,22 @@ document.addEventListener('DOMContentLoaded', () => {
   
     // Trigger the form submission
     formFilter.dispatchEvent(new Event('submit'));
+//********************************************************************************
+/// Generate PDF to print
+//********************************************************************************
+    pdfButton.addEventListener('click', function() {
+        const elementsToPrint = document.getElementById('document');
+
+        const filename = `${pdfFileName}.pdf`;
+
+        const options = {
+            filename: filename,
+            image: { type: 'jpeg', quality: 0.98 },
+            html2canvas: { scale: 2 },
+            jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+        };
+        html2pdf().set(options).from(elementsToPrint).save();
+    });
 //********************************************************************************
 ///End
 //********************************************************************************
